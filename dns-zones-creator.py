d1a2bd295ae89e1a81d4ca5c77cd16a38cc5cd25
JB <35406993+cherokeejb@users.noreply.github.com>
2018-01-14 02:11:53 +0100

    Create dns-zones-creator.py
    *LICENSE EXCEPTION : this file despite continued further modification by me in my learning process is mostly from professor Mike O'Leary (Townsend / "Cyber Operations...") and should be treated as such.  This is here to demonstrate my learning and reflection of common dns forward and reverse setup scripting, and to experiment with getting out of 'the bash comfort zone'
    
    The ultimate goal would be to be able to reproduce or more importantly, similarly productive scripts, spontaneously, with little guide
    
    This was a fun lab exercize, and great for learning the dns/BIND service in depth
    
    Mike O'Leary states in "Cyber Operations,..." "This program reads each line from stars.csv, storing the host name and the IP address. It builds the corresponding FQDN and uses that to write the matching address record line to forward.txt. It splits the IP address up into octets, reverses them adding “.in-addr.arpa.” to build the pointer record, which is written to reverse.txt. The resulting files can then be copied and pasted into appropriate zone files.   ...The advantage of this approach is that it avoids typographical errors and ensures consistency between the data in the forward and reverse zone files."

---
+++ dns-zones-creator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/pythonimport
+
+#This imports the data listed in a csv file, and correcctly formats it ready for the forward and reverse zone files.
+
+#   #Pay especially to the reverse naming syntax for the reverse files, essentially is the address backwards followed by '.in-addr.arpa.' including the trailing period
+    #Zone files can be named as desired, but as outlined in your named.conf file, created in the BIND install
+
+#Don't forget to look over the results, and as well, create a loopback zone as needed (e.g. for names like 'localhost')
+
+
+#By the way, comments in python using """ multi-line comment """ are actually just strings headed for garbage collection :)!
+    #Thinkaboutit
+
+
+
+#
+#----------------------------- example input file-------
+#This script reads from a file, here scan.csv, which contains a list of host/results formatted as below (remove leading hash and tabs):
+#   Host1,192.168.100.101
+#   Host2,192.168.100.1
+#   Host3,172.12.1.2
+#--------------------------------------------------------
+
+#define your variable setup
+#Note that with some planning or in cetain setups, the forward and reverse files could also be correctly named here
+
+input_file_name = "scan.csv"           #Main line-separated file of hostname,ip address for each hosts
+forward_file_name = "forward.txt"
+reverse_file_name = "reverse.txt"
+domain_name = ".desired.domain."       #don't forget the trailing . after the end of the fqdn
+
+# data preparation
+# pulls in data ready for import below, open command
+input_file = open(input_file_name,'r')
+forward_file = open(forward_file_name,'w')
+reverse_file = open(reverse_file_name,'w')
+
+
+#Calls module input_reader and loads csv import using the 
+input_reader = csv.reader(input_file)
+
+#Here the files are formatted correctly for forward and reverse zones, showing pythons text processing ease
+for line in input_reader:   
+    host = line[0]
+     ip = line[1]
+     fqdn = host +  domain_name
+     padding = ' ' * (30 - len(fqdn))
+     forward_file.write(fqdn + padding + 'IN A     ' + ip + '\n')
+     [i1,i2,i3,i4] = ip.split('.')
+     revaddr = i4 + '.' + i3 + '.' + i2 + '.' + i1 + '.in-addr.arpa.'
+     padding = ' ' * (30 - len(revaddr))
+     reverse_file.write(revaddr + padding + 'IN PTR   ' + fqdn + '\n')
